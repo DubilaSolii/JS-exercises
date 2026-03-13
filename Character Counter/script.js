8091b64fd589b5ae6991b046a66c95052d1f1368
DubilaSolii <dubilasoliiberinyuy@gmail.com>
2026-03-13 08:51:20 +0000

    Updated script.js
    from the teacher

--- Character Counter/script.js
+++ Character Counter/script.js
@@ -1,7 +1,8 @@
 // function counter(){
     let textarea=document.getElementById('text');
     textarea.addEventListener('input',function(){
-        document.getElementsByClassName('char')[0].innerText=textarea.value.length;
+        let resultElt= document.getElementsByClassName('char')[0];
+       resultElt.innerrText=textarea.value.length;
         console.log(textarea.value.length);
     })
     // character.innerText= textarea.length;
